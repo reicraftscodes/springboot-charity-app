649144d49dce4d50fb8cb2d38aead7a14638c022
c1623793 <sanejolm@cardiff.ac.uk>
2019-11-08 23:27:39 +0000

    added new test to see a sponsor's page

--- src/test/java/com/nsa/charitystarter/context/SponsorTest.java
+++ src/test/java/com/nsa/charitystarter/context/SponsorTest.java
@@ -83,5 +83,42 @@ public class SponsorTest {
                 );
     }
 
+    @Test
+    public void gotoCollySponsor() throws Exception {
+        SponsorPageCreated ccohalane2Sponsor = new SponsorPageCreated(
+                611, "Colly Cohalan", "8", "Etiam pretium iaculis justo. In hac habitasse platea dictumst. Etiam faucibus cursus urna. Ut tellus. Nulla ut erat id mauris vulputate elementum. Nullam varius. Nulla facilisi. Cras non velit nec nisi vulputate nonummy. Maecenas tincidunt lacus at velit. Vivamus vel nulla eget eros elementum pellentesque.",
+                LocalDateTime.of(2017, 8, 9, 0, 0),
+                LocalDateTime.of(2016, 11, 14, 0, 0),
+                LocalDateTime.of(2017, 4, 18, 0, 0),
+                "ccohalane2");
+
+        List<DonationSummary> listSum = List.of(new DonationSummary("fName", "lname", 12d,
+                LocalDateTime.of(2017, 8, 9, 0, 0)));
+
+        SponsorDonationInfo sponsorDonationStats = new SponsorDonationInfo("title", "ccohalane2", 12d, 2d);
+
+        given(this.sponsorFinder.findSponsorByFURL("ccohalane2")).willReturn(Optional.of(ccohalane2Sponsor));
+        given(this.donationFinder.findLastDonationsBySponsor(ccohalane2Sponsor.getId())).willReturn(listSum);
+        given(this.donationFinder.getSponsorTotalDonationsInfo(ccohalane2Sponsor.getId())).willReturn(sponsorDonationStats);
+
+
+        mvc.perform(
+                get
+                        ("/sponsorship/ccohalane2")
+        )
+                .andDo(
+                        print()
+                )
+                .andExpect(
+                        status().isOk()
+                )
+                .andExpect(
+                        content().string(
+                                containsString("Colly Cohalan")
+                        )
+                );
+
+    }
+
 
 }
